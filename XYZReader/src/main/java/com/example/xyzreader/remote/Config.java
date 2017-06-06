095d4a55bdfcb6fcc7148c3eb2005dcbe640eb6f
gustavoballeste <gustavoballeste@gmail.com>
2017-06-05 23:20:04 -0300

    fix: Set alternative url for Udacity JSON file

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -3,13 +3,18 @@ package com.example.xyzreader.remote;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/** Using my own alternate url which has some extra goodies
+ *
+ * Default address by udacity -
+ * https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json
+ * */
 public class Config {
     public static final URL BASE_URL;
 
     static {
         URL url = null;
         try {
-            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+            url = new URL("https://github.com/gustavoballeste/dump/blob/master/Lego/data.json" );
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
